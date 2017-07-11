b173f90f4e7fdaccd20e7e20e647175b36db77be
Haixiang <yan6123@qq.com>
2017-07-11 09:03:24 +0800

    更新了UI库

---
+++ frontend/src/components/pages/Home/Recommend/Recommend.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="yan-recommend-wrapper">
+        <!--简介部分-->
+        <div class="yan-recommand-des">
+            <span>
+                <i class="fa fa-check-square-o" aria-hidden="true"></i>
+                <span>网易自营品牌</span>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+/*简介部分*/
+
+.yan-recommand-des {
+    margin-top: 100px;
+}
+
+.yan-recommand-des i {
+    color: rgb(180, 40, 45);
+}
+</style>
